42ddd95150cba2416a2264213073ab112da2ef4e
Ahmed-Morsi888 <morsia531@gmail.com>
2026-02-19 02:44:50 +0200

    navbar become ready

--- src/app/Core/navbar/navbar.ts
+++ src/app/Core/navbar/navbar.ts
@@ -1,16 +1,9 @@
-import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { User } from '../../user/user';
-import { Admin } from '../../admin/admin';
-import { AuthService } from '../../auth-service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {
-  _authService = inject(AuthService);
-  islogged: boolean = this._authService.isloggedin();
-}
+export class Navbar {}
